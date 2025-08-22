e2d2a0b30bd5644bd439953ba4c1be681a5ac463
Daniel <daniel@blacklightsolutions.com>
2025-08-21 19:49:14 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_KINDS,ASTEROID_SPAWN_RATE, SCREEN_HEIGHT, SCREEN_WIDTH
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill('black')
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+if __name__ == "__main__":
+    main()
