233173fd0439d8b6affbfe4629dd4646b3ef5026
Daniel <daniel@blacklightsolutions.com>
2025-08-21 20:41:00 -0500

    Added player containers.

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import pygame
-from constants import ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_KINDS,ASTEROID_SPAWN_RATE, SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
-        screen.fill('black')
-        pygame.display.flip()
+    clock = pygame.time.Clock()
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
